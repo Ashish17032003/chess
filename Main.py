46436422ff181cb27ad1f98212366132c5219d6c
Ashish17032003 <ashishkv02@gmail.com>
2023-03-25 20:39:24 +0530

    Board Check

--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
 import pygame as p
 import Engine
 
-p.init()
 WIDTH = HEIGHT = 512  # 400 is another option
-
 DIMENSION = 8  # dimensions of a chess board are 8x8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15  # for animations later on
@@ -23,6 +21,11 @@ def loadImages():
 # Note: we can access an image by saying "IMAGES['wp']"
 
 def main():
+    """
+    The main driver for our code.
+    This will handle user input and updating the graphics.
+    """
+    p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
@@ -36,6 +39,45 @@ def main():
 
         clock.tick(MAX_FPS)
         p.display.flip()
+
+
+'''
+Responsible for all the graphics within a current game state.
+'''
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)  # draw squares on the board
+    # add in piece highlighting or move suggestions (later)
+    drawPieces(screen, gs.board)  # draw pieces on top of those squares
+
+
+'''
+Draw the squares on the board. The top left square is always light.
+'''
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            # dark squares remainder 1 and light squares remainder 0
+            color = colors[((r + c) % 2)]
+            # drawing column by row
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+'''
+Draw the pieces on the board using the current GameState.board
+'''
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":  # not an empty square
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == '__main__':
