ab465e0d824370f0236a693c5f508d990a219ba4
Ashish17032003 <ashishkv02@gmail.com>
2023-03-26 16:29:13 +0530

    makes moves like kids

--- Engine.py
+++ Engine.py
@@ -1,4 +1,4 @@
-class GameState():
+class GameState:
     def __init__(self):
         # Board is a 8x8 2-dimensional list where each element of the list contains 2 characters
         # The first character represents the color of the piece, "b" or "w"
@@ -15,7 +15,38 @@ class GameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
 
-    
+        self.whiteToMove = True
+        self.moveLog = []
 
-        
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)  # log the move so we can undo it later
+        self.whiteToMove = not self.whiteToMove  # swap players
 
+
+class Move:
+    # in chess, fields on the board are described by two symbols, one of them being number between 1-8 (which is
+    # corresponding to rows) and the second one being a letter between a-f (corresponding to columns), in order to
+    # use this notation we need to map our [row][col] coordinates to match the ones used in the original chess game
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        # you can add to make this like real chess notation
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- Main.py
+++ Main.py
@@ -38,12 +38,35 @@ def main():
     gs = Engine.GameState()
     loadImages()  # only do this once before the while loop
     running = True
+    sqSelected = ()  # no square is selected, keep track of the last click of the user (tuple: (row,col))
+    playerClicks = []  # keeps track of player clicks ( two tuples : [(6,4) , (4,4)] ) ( moving the white pawn ....)
+
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
 
-        drawGameState(screen, gs)   
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x,y) location of mouse
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                # we know where we clicked now ( i.e. what piece we have selected )
+                if sqSelected == (row, col):  # the user clicked the same square twice
+                    sqSelected = ()  # deselect i.e. empty
+                    playerClicks = []  # clear player clicks
+
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)  # append for both 1st and 2nd clicks
+
+                if len(playerClicks) == 2:  # after 2nd click
+                    move = Engine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()  # reset user clicks
+                    playerClicks = []
+
+        drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -84,7 +107,7 @@ def drawPieces(screen, board):
         for c in range(DIMENSION):
             piece = board[r][c]
             if piece != "--":  # not an empty square
-                screen.blit( IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == '__main__':
